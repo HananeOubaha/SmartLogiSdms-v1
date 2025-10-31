0442425ef226db3d7f49a7c8e6f7cfb8623e99d3
Hanane Oubaha <Hananeouba03@gmail.com>
2025-10-31 15:03:12 +0100

    feat : j'ai cree mapper pour crud de l'entité zone  https://hananeoubaha.atlassian.net/jira/software/projects/SDMSV1/list/?jql=project+%3D+%22SDMSV1%22+ORDER+BY+created+DESC&selectedIssue=SDMSV1-2&atlOrigin=eyJpIjoiZjZkYzg0YjExNDcxNGE4Mzg4MjA0ZWE3ZDcyN2Q1Y2QiLCJwIjoiaiJ9

---
+++ src/main/java/com/smartlogi/sdms/mapper/ZoneMapper.java
@@ -0,0 +1,26 @@
+package com.smartlogi.sdms.mapper;
+
+import com.smartlogi.sdms.dto.ZoneDto;
+import com.smartlogi.sdms.model.Zone;
+import org.mapstruct.Mapper;
+
+/**
+ * Interface MapStruct pour la conversion entre Zone et ZoneDto.
+ */
+@Mapper(componentModel = "spring") // componentModel="spring" rend ce mapper injectable (via @Autowired)
+public interface ZoneMapper {
+
+    /**
+     * Convertit une entité Zone en ZoneDto.
+     * @param zone L'entité à convertir.
+     * @return Le DTO correspondant.
+     */
+    ZoneDto toDto(Zone zone);
+
+    /**
+     * Convertit un ZoneDto en entité Zone.
+     * @param zoneDto Le DTO à convertir.
+     * @return L'entité correspondante.
+     */
+    Zone toEntity(ZoneDto zoneDto);
+}
