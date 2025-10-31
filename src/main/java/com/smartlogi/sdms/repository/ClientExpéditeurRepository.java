46fe01b3200687101eb3aecefd7fe924ae6405e4
Hanane Oubaha <Hananeouba03@gmail.com>
2025-10-31 16:57:02 +0100

    feat : j'ai creer repository pour client expéditeur  cl https://hananeoubaha.atlassian.net/browse/SDMSV1-3?atlOrigin=eyJpIjoiNzNhNzQzYjY2OTA0NGQ4YzkxZDY4MzU4NjkxOTA2MWYiLCJwIjoiaiJ9

---
+++ src/main/java/com/smartlogi/sdms/repository/ClientExpéditeurRepository.java
@@ -0,0 +1,14 @@
+package com.smartlogi.sdms.repository;
+
+import com.smartlogi.sdms.model.ClientExpéditeur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientExpéditeurRepository extends JpaRepository<ClientExpéditeur, Long> {
+
+    // Ajout d'une méthode pour vérifier l'unicité par email, très utile.
+    Optional<ClientExpéditeur> findByEmail(String email);
+}
