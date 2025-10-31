b8bf82d4bebb37b75c4db938689b5b621a83debf
Hanane Oubaha <Hananeouba03@gmail.com>
2025-10-31 16:56:52 +0100

    feat : j'ai creer Mapper pour client expéditeur  cl https://hananeoubaha.atlassian.net/browse/SDMSV1-3?atlOrigin=eyJpIjoiNzNhNzQzYjY2OTA0NGQ4YzkxZDY4MzU4NjkxOTA2MWYiLCJwIjoiaiJ9

---
+++ src/main/java/com/smartlogi/sdms/mapper/ClientExpéditeurMapper.java
@@ -0,0 +1,13 @@
+package com.smartlogi.sdms.mapper;
+
+import com.smartlogi.sdms.dto.ClientExpéditeurDto;
+import com.smartlogi.sdms.model.ClientExpéditeur;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ClientExpéditeurMapper {
+
+    ClientExpéditeurDto toDto(ClientExpéditeur clientExpéditeur);
+
+    ClientExpéditeur toEntity(ClientExpéditeurDto clientExpéditeurDto);
+}
