8270f847e546889ed09f6bd4a6fb13f223857e8b
Hanane Oubaha <Hananeouba03@gmail.com>
2025-10-31 15:01:23 +0100

    feat : j'ai cree dto pour crud de l'entité zone  https://hananeoubaha.atlassian.net/jira/software/projects/SDMSV1/list/?jql=project+%3D+%22SDMSV1%22+ORDER+BY+created+DESC&selectedIssue=SDMSV1-2&atlOrigin=eyJpIjoiZjZkYzg0YjExNDcxNGE4Mzg4MjA0ZWE3ZDcyN2Q1Y2QiLCJwIjoiaiJ9

---
+++ src/main/java/com/smartlogi/sdms/DTO/ZoneDto.java
@@ -0,0 +1,22 @@
+package com.smartlogi.sdms.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ZoneDto {
+
+    private Long id; // ID pour les opérations d'UPDATE/DELETE
+
+    @NotBlank(message = "Le nom de la zone est obligatoire")
+    @Size(max = 100, message = "Le nom ne doit pas dépasser 100 caractères")
+    private String nom;
+
+    @Size(max = 20, message = "Le code postal ne doit pas dépasser 20 caractères")
+    private String codePostal;
+}
